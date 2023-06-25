ad829d10676ba1558a50cca3bc9813037d312efd
Paula C <bakerp24@gmail.com>
2023-06-25 08:28:11 -0500

    initial commit. adding first exercise result

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputElement = () => {
-  return <input placeholder="Enter Some Text" />;
+  const [inputText, setInputText] = useState("");
+  const [historyList, setHistoryList] = useState([]);
+
+  return <div><input onChange = {(e) => {
+    setInputText(e.target.value)
+    setHistoryList([...historyList, e.target.value])
+  }} 
+  placeholder="Enter Some Text" />{inputText}
+  <hr /><br />
+    <ul>
+      {historyList.map((history) => {
+        return <li>{history}</li>
+      })}
+    </ul>
+  </div>
 };
 
 export default InputElement;
