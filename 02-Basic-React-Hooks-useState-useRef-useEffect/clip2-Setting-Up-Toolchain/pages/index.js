98cce13d3e74db51f755f2b3cdaf7e345c092089
Paula C <bakerp24@gmail.com>
2023-07-03 14:18:48 -0500

    the wrong way to call a hook

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const InputElement = () => {
   const [inputText, setInputText] = useState("");
   const [historyList, setHistoryList] = useState([]);
+  const random_boolean = Math.random() >= 0.5;
+  //const [ isLoading, setIsLoading ] = useState(true);
 
-  return <div><input onChange = {(e) => {
+  // cannot call a hook from the inside of a conditional
+  // if (random_boolean === true){
+  //   const [ isLoading, setIsLoading ] = useState(true);
+  // } else{
+  //   const [ isLoading, setIsLoading ] = useState(false);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  });
+
+  return isLoading ? <div>Loading...</div> : <div><input onChange = {(e) => {
     setInputText(e.target.value)
     setHistoryList([...historyList, e.target.value])
   }} 
